b15a6746e9310fea5d9dc43fb86e2ba5d777fd49
mohamedelserafy <elserafy02@gmail.com>
2021-04-14 02:39:51 +0200

    course router

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
-const express = require('express')
-const router = require('./routes')
+const express = require("express");
+const router = require("./routes");
+const courseRouter = require("./routes/course");
 
-const session = require('./middleware/session ')
+const session = require("./middleware/session ");
 
-const corsMw =require('./middleware/cors')
-const apiErrorHandler =require('./errors/api-error-handler')
+const corsMw = require("./middleware/cors");
+const apiErrorHandler = require("./errors/api-error-handler");
 
 const dbSetup = require("./db/db-setup");
 dbSetup();
 
-
-const app = express()
+const app = express();
 
 //if the app is running with a proxy like (nginx)
 //app.set('trust proxy',1)
 
 //
-app.use(express.json())
-
+app.use(express.json());
 
 //setup CORS login
-app.options('*', corsMw)
-app.use(corsMw)
+app.options("*", corsMw);
+app.use(corsMw);
 
-app.use(session)
+app.use(session);
 
+app.use("/course", courseRouter);
 
-app.use(router)
+app.use(router);
 
-app.use(apiErrorHandler)
+app.use(apiErrorHandler);
 
-
-
-
-
-app.listen(8080,()=>console.log('server is running on port 8080'))
+app.listen(8088, () => console.log("server is running on port 8080"));
