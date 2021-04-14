ae4caf0587bc988ff91cee2a5b3b930b0697262d
mohamedelserafy <elserafy02@gmail.com>
2021-04-14 02:37:08 +0200

    feedback model

--- db/models/Feedback.js
+++ db/models/Feedback.js
@@ -4,5 +4,26 @@ class Feedback extends Model {
   static get tableName() {
     return "feedback";
   }
+
+  static get relationMappings() {
+    return {
+      comments: {
+        relation: Model.HasManyRelation,
+        modelClass: require("./comment"),
+        join: {
+          from: "feedback.id",
+          to: "comment.feedback_id",
+        },
+      },
+      student: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: require("/user"),
+        join: {
+          from: "feedback.student_id",
+          to: "user.id",
+        },
+      },
+    };
+  }
 }
 module.exports = Feedback;
